253961a0382aeb479d3718bd0ce098e400304061
FrancyelZelling <zellingf@gmail.com>
2019-12-15 17:17:53 -0300

    Getting Started done

---
+++ data.py
@@ -0,0 +1,25 @@
+def articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body' : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum asperiores sit deleniti at mollitia, consequatur corrupti unde! Exercitationem quasi, hic fuga nobis dolorem quos quibusdam nesciunt porro dicta recusandae!',
+            'Author': 'Francyel Zelling',
+            'Created_at': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body' : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum asperiores sit deleniti at mollitia, consequatur corrupti unde! Exercitationem quasi, hic fuga nobis dolorem quos quibusdam nesciunt porro dicta recusandae',
+            'Author': 'Francyel Zelling',
+            'Created_at': '04-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body' : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem cum asperiores sit deleniti at mollitia, consequatur corrupti unde! Exercitationem quasi, hic fuga nobis dolorem quos quibusdam nesciunt porro dicta recusandae!',
+            'Author': 'Francyel Zelling',
+            'Created_at': '04-25-2017'
+        },
+    ]
+    return articles
